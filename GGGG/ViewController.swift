e469f801846695c8f043f4f486edcf3784f786fe
CocoaHaha <1076800596@qq.com>
2017-06-05 14:43:45 +0800

    Initial Commit

---
+++ GGGG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GGGG
+//
+//  Created by 石向锋 on 2017/6/5.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
