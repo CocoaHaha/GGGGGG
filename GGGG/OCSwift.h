afbe5533fa94dd7d46daf566318356d7e0cad9fd
CocoaHaha <1076800596@qq.com>
2017-06-20 11:57:13 +0800

    first commit

---
+++ GGGG/OCSwift.h
@@ -0,0 +1,13 @@
+//
+//  OCSwift.h
+//  GGGG
+//
+//  Created by 石向锋 on 2017/6/5.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCSwift : NSObject
+
+@end
---
+++ GGGG/OCSwift.m
@@ -0,0 +1,13 @@
+//
+//  OCSwift.m
+//  GGGG
+//
+//  Created by 石向锋 on 2017/6/5.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import "OCSwift.h"
+
+@implementation OCSwift
+
+@end
